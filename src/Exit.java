4b273a0aa8aaef8a049cbac3470de28c385e1d51
Kernel-rb <matabsaifeddine@gmail.com>
2024-04-20 19:41:53 +0100

    ☕

--- src/Exit.java
+++ src/Exit.java
@@ -1,8 +1,14 @@
 package src;
 
 public class Exit implements Command {
+
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    
     @Override 
     public void oper(Database database , User user){
-        System.out.println("Add Book");
+        Logger.log("Exiting the program");
+        System.out.println(ANSI_GREEN + "Goodbye" + ANSI_RESET);
+        System.exit(0);
     }
 }
